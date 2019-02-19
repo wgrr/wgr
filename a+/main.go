f1b195fad2e7129967ec38f0e18d50824dd196e8
Wagner Riffel <wgrriffel@gmail.com>
2019-02-19 03:54:32 -0300

    a: fix same bug it had with 9 sed, always inserting newline after eof

--- a+/main.go
+++ a+/main.go
@@ -2,13 +2,40 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
 
 func main() {
-	bio := bufio.NewScanner(os.Stdin)
-	for bio.Scan() {
-		fmt.Printf("\t%s\n", bio.Text())
+	s := bufio.NewScanner(os.Stdin)
+	s.Split(scan)
+	bo := bufio.NewWriter(os.Stdout)
+	defer bo.Flush()
+	for s.Scan() {
+		err := bo.WriteByte('\t')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "write: %v\n", err)
+			continue
+		}
+		_, err = bo.WriteString(s.Text())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "write: %v\n", err)
+			continue
+		}
 	}
+}
+
+func scan(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		i++
+		return i, data[0:i], nil
+	}
+	if atEOF {
+		return len(data), data, nil
+	}
+	return 0, nil, nil
 }
--- a-/main.go
+++ a-/main.go
@@ -2,20 +2,41 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
 
 func main() {
-	bio := bufio.NewScanner(os.Stdin)
-	for bio.Scan() {
-		l := bio.Text()
-		// instead of this logic, i could just implement
-		// a SpliFunc to scanner, just don't bother for now.
-		if len(l) > 0 && l[0] == '\t' {
-			fmt.Printf("%s\n", l[1:])
-		} else {
-			fmt.Printf("%s\n", l)
+	s := bufio.NewScanner(os.Stdin)
+	s.Split(scan)
+	bo := bufio.NewWriter(os.Stdout)
+	defer bo.Flush()
+	for s.Scan() {
+		_, err := bo.WriteString(s.Text())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "write: %v\n", err)
+			continue
 		}
 	}
+}
+
+func scan(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		i++
+		if data[0] == '\t' {
+			return i, data[1:i], nil
+		}
+		return i, data[0:i], nil
+	}
+	if atEOF {
+		if data[0] == '\t' {
+			return len(data), data[1:], nil
+		}
+		return len(data), data, nil
+	}
+	return 0, nil, nil
 }
